b6f0ea819cbe511f861512dcab37e63b5f97ed96
Teotia444 <comptest444@gmail.com>
2026-06-21 15:30:44 +0200

    github desktop nonsense

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
 #include "main.h"
-#include "TCPGecko.hpp"
+#include "TCPGecko.h"
 #include "utils/logger.h"
 
+#include <coreinit/filesystem.h>
 #include <wups.h>
-<<<<<<< Updated upstream
-#include <utils/CThread.h>
-=======
 #include <wups/config_api.h>
 #include <malloc.h>
 #include <thread>
->>>>>>> Stashed changes
+#include <coreinit/thread.h>
 
 /**
     Mandatory plugin information.
@@ -17,14 +15,11 @@
 **/
 WUPS_PLUGIN_NAME("TCPGecko");
 WUPS_PLUGIN_DESCRIPTION("Port of the TCPGecko features over to an aroma plugin.");
-WUPS_PLUGIN_VERSION("v0.2");
+WUPS_PLUGIN_VERSION("v0.1");
 WUPS_PLUGIN_AUTHOR("Teotia444");
+WUPS_USE_WUT_DEVOPTAB();                // Use the wut devoptabs
 WUPS_USE_STORAGE("tcpgecko"); // Unique id for the storage api
 
-<<<<<<< Updated upstream
-OSThread* mainThread = NULL;
-TCPServer* socketThread = NULL;
-=======
 WUPSConfigAPICallbackStatus ConfigMenuOpenedCallback(WUPSConfigCategoryHandle rootHandle) {
     //maybe add something to the config menu from aroma in the future?
     return WUPSCONFIG_API_CALLBACK_RESULT_SUCCESS;
@@ -45,20 +40,11 @@ INITIALIZE_PLUGIN() {
     initLogging();
     DEBUG_FUNCTION_LINE_INFO("Initialized TCPGecko Plugin");
 }
->>>>>>> Stashed changes
 
 ON_APPLICATION_START() {
     initLogging();
     DEBUG_FUNCTION_LINE_INFO("TCPGecko Plugin Application start");
     
-<<<<<<< Updated upstream
-    mainThread = OSGetCurrentThread();
-    socketThread = new TCPServer(7332);
-}
-
-ON_APPLICATION_REQUESTS_EXIT(){
-    delete socketThread;
-=======
     ct = OSGetCurrentThread();
     
     thread = new std::jthread(Start);
@@ -68,12 +54,11 @@ ON_APPLICATION_REQUESTS_EXIT(){
 ON_APPLICATION_REQUESTS_EXIT(){
     thread->request_stop();
     thread->join();
-    DEBUG_FUNCTION_LINE_INFO("sure ig bro");
 }
 
 DEINITIALIZE_PLUGIN() {
->>>>>>> Stashed changes
     deinitLogging();
 }
+
 
 
--- src/main.h
+++ src/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
+#include <coreinit/thread.h>
 
-#include "TCPGecko.hpp"
-#include "utils/CThread.h"
-
-extern OSThread* mainThread;
-extern TCPServer* socketThread;
+OSThread* GetMainThread();
 
 #endif
